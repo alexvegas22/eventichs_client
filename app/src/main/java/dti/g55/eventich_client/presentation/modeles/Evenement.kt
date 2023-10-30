69d50282576e0ab64a7c2e6146014ed72d5148c5
MrPhilWorld <phillafrance10@gmail.com>
2023-10-30 15:55:04 -0400

    Ajout de mon éran liste d'événements

---
+++ app/src/main/java/dti/g55/eventich_client/presentation/modeles/Evenement.kt
@@ -0,0 +1,4 @@
+package dti.g55.eventich_client.presentation.modeles
+
+class Evenement {
+}
---
+++ app/src/main/java/dti/g55/eventich_client/presentation/modeles/EvenementListeItem.kt
@@ -0,0 +1,7 @@
+package dti.g55.eventich_client.presentation.modeles
+
+import java.util.Date
+
+class EvenementListeItem(val imageId: Int, val nom: String, val date: Date, val location: String) {
+
+}
