f7c7864c331f30cd0093eeb1a5f4512a265bfd1a
MrPhilWorld <phillafrance10@gmail.com>
2023-10-30 14:42:47 -0400

    Ajout du nouveau projet avec la structure de dossier de départ et le fragment container view pour activity_main

---
+++ app/src/main/java/dti/g55/eventich_client/MainActivity.kt
@@ -0,0 +1,11 @@
+package dti.g55.eventich_client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
