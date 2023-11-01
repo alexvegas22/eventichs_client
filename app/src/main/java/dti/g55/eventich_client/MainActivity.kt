aa92230f4c8d94f536f09ef8f43e28de5ba1dafa
Alexandre Baudouin Vegas <alexrbvegas@gmail.com>
2023-10-31 23:03:58 -0400

    pull des autres branches

--- app/src/main/java/dti/g55/eventich_client/MainActivity.kt
+++ app/src/main/java/dti/g55/eventich_client/MainActivity.kt
@@ -2,10 +2,39 @@ package dti.g55.eventich_client
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+    lateinit var fragmentContainerView: FragmentContainerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fragmentContainerView = findViewById(R.id.fragmentContainerView)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.bottom_navigation_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item_accueil -> {
+                println("THIS IS A TEST -----------------------------------------------------")
+                fragmentContainerView.findNavController().navigate(R.id.action_aller_accueil)
+                return true
+            }
+            R.id.item_recherche -> {
+                fragmentContainerView.findNavController().navigate(R.id.action_aller_recherche)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
