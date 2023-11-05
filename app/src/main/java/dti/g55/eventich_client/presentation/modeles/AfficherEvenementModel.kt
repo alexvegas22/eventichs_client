4a10a714ecf7b5559b2c4a793050d8a9d72da2b8
MrPhilWorld <phillafrance10@gmail.com>
2023-11-04 20:30:40 -0400

    Fixer le problème avec AfficherEvenementModel utilisant la vieille entité Evenement

--- app/src/main/java/dti/g55/eventich_client/presentation/modeles/AfficherEvenementModel.kt
+++ app/src/main/java/dti/g55/eventich_client/presentation/modeles/AfficherEvenementModel.kt
@@ -1,8 +1,10 @@
 package dti.g55.eventich_client.presentation.modeles
 
+import dti.g55.eventich_client.R
 import dti.g55.eventich_client.domaine.entite.Evenement
+import java.util.Date
 
-val testEvenement = Evenement("TEST")
+val testEvenement = Evenement(R.drawable.ic_search, "Événement", Date(), "123 rue Chemin, Montréal, QC")
 class AfficherEvenementModel(var événement: Evenement = testEvenement) {
 
 }
