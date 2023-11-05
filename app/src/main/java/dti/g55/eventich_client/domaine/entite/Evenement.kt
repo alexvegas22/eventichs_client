44b9e61a2c13138fc206aa280314053af71a4bff
Alexandre Baudouin Vegas <abaudouinvegas@dept-info.crosemont.quebec>
2023-11-05 06:36:58 +0000

    Update Evenement.kt

--- app/src/main/java/dti/g55/eventich_client/domaine/entite/Evenement.kt
+++ app/src/main/java/dti/g55/eventich_client/domaine/entite/Evenement.kt
@@ -2,5 +2,5 @@ package dti.g55.eventich_client.domaine.entite
 
 import java.util.Date
 
-class Evenement(val imageId: Int, val nom: String, val date: Date, val location: String) {
+class Evenement(val imageId: Int, val nom: String, val date: Date, val location: String, val organisation : String)  {
 }
