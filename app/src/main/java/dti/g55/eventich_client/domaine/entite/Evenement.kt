14e7ba6e6184018a2aa82c04c956f50de6741827
elies <eboudida@gmail.com>
2023-11-06 15:01:40 -0500

    modification entité Evenement

--- app/src/main/java/dti/g55/eventich_client/domaine/entite/Evenement.kt
+++ app/src/main/java/dti/g55/eventich_client/domaine/entite/Evenement.kt
@@ -2,6 +2,6 @@ package dti.g55.eventich_client.domaine.entite
 
 import java.util.Date
 
-class Evenement(val imageId: Int, val nomComplet: String, val nomAbbrevié: String, val nbParticipant: Int,  val date: Date, val location: String, val organisation : String,
-    val catégorie: String, val organisateur: String, val description: String, val meteo: ConditionMeterologique)  {
+class Evenement(val imageId: Int, val nomComplet: String, val nbParticipant: Int,  val date: Date, val location: String, val organisation : String,
+    val catégorie: String, val description: String)  {
 }
