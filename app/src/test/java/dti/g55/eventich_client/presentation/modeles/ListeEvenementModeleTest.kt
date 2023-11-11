fda3f5648b8344228987e836235f5b005343bcd2
plafrance1 <phillafrance10@gmail.com>
2023-11-11 17:53:19 -0500

    Fail d'un unit test pour vérifier l'étape de test de gitlab-ci

--- app/src/test/java/dti/g55/eventich_client/presentation/modeles/ListeEvenementModeleTest.kt
+++ app/src/test/java/dti/g55/eventich_client/presentation/modeles/ListeEvenementModeleTest.kt
@@ -1,9 +1,11 @@
 package dti.g55.eventich_client.presentation.modeles
 
+import org.junit.Assert.*
 import org.junit.Test
 
 class ListeEvenementModeleTest {
     @Test
     fun `Étant donné un modèle liste événement nouvellement initialisé, lorsqu'on retourne la liste d'événements, on obtient 1 élément`() {
+        assertEquals(4, 2 + 3)
     }
 }
